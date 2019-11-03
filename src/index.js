44534972769dfeaf5676a6fcd3759e3607e6293f
Hrugved <45076119+Hrugved@users.noreply.github.com>
2019-11-03 23:45:57 +0530

    Add socket initial config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
-
+const server = http.createServer(app)
+const io = socketio(server)
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(publicDirectoryPath))
 
-app.listen(process.env.PORT, () => {
+io.on('connection', () => {
+    console.log('New Websocket connection')
+})
+
+server.listen(process.env.PORT, () => {
     console.log(`Server is up and running on port ${process.env.PORT}`)
 })
