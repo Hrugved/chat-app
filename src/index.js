d9de03b4bc6829d4583e6951d6ee878426a15919
Hrugved <45076119+Hrugved@users.noreply.github.com>
2019-11-04 08:24:59 +0530

    L155 - basic socket

--- src/index.js
+++ src/index.js
@@ -12,8 +12,18 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0;
+
+// runs for every client
+io.on('connection', (socket) => {
     console.log('New Websocket connection')
+    // emiting on 'socket' sends it to this single connected client 
+    socket.emit('countUpdated', count) // want to send initial count only to newly connected client
+    socket.on('increment', () => {
+        count++
+        // emiting on 'io' sends it to the all connected client
+        io.emit('countUpdated',count) // want to send updated count to all connected clients
+    })
 })
 
 server.listen(process.env.PORT, () => {
